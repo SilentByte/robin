94ec0341c48f229e07bc351cc20328a745518cb5
Rico A. Beti <rico.beti@silentbyte.com>
2020-06-20 22:05:06 +0800

    Use custom logger due to issues with Firebase

--- functions/src/log.ts
+++ functions/src/log.ts
@@ -3,9 +3,41 @@
  * Copyright (c) 2020 by SilentByte <https://www.silentbyte.com/>
  */
 
-import * as log from "console-log-level";
+const LOG_LEVELS = {
+    "debug": 0,
+    "info": 1,
+    "warn": 2,
+    "error": 3,
+};
 
-export default log({
-    level: "info",
-});
+class Logger {
+    constructor(private level: keyof typeof LOG_LEVELS) {
+        //
+    }
 
+    debug(object: any): void {
+        if(LOG_LEVELS[this.level] <= LOG_LEVELS.debug) {
+            console.debug(JSON.stringify(object));
+        }
+    }
+
+    info(object: any): void {
+        if(LOG_LEVELS[this.level] <= LOG_LEVELS.info) {
+            console.info(JSON.stringify(object));
+        }
+    }
+
+    warn(object: any): void {
+        if(LOG_LEVELS[this.level] <= LOG_LEVELS.warn) {
+            console.warn(JSON.stringify(object));
+        }
+    }
+
+    error(object: any): void {
+        if(LOG_LEVELS[this.level] <= LOG_LEVELS.error) {
+            console.error(JSON.stringify(object));
+        }
+    }
+}
+
+export default new Logger("info");
